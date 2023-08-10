56494093e0a8184b8a4420444b31954847b3709f
xoffio <marquessricardo@gmail.com>
2023-08-09 22:19:53 -0400

    initial server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+//use std::collections::HashMap;
+
+use axum::{extract::Path, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/:version/*path", get(gate_get));
+
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn gate_get(Path((version, api_path)): Path<(String, String)>) -> String {
+    format!(" version: {}, \n Path: {}", version, api_path)
 }
